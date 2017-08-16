fb591f1236d048f1127edd14e8a3abc3d4753318
mpenna <tworker@onyx.ove.com>
2017-08-16 11:18:14 -0400

    Added User create test mock endpoint

--- endpoints/users.rb
+++ endpoints/users.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require 'factory_girl'
 require_relative 'support/env'
+require 'uri'
 
 class UsersApp < Sinatra::Base
 
@@ -51,6 +52,48 @@ class UsersApp < Sinatra::Base
     jwtSubject = FactoryGirl.build(:jwtsubject, href: user.href, companyHref: company.href, firstName: user.firstName, lastName: user.lastName, role: user.role, userName: user.userName, emailAddress: user.emailAddress)
 
     [200, {'Content-Type'=>'application/json'}, jwtSubject.to_json]
+  end
+
+  get '/users/clear' do
+    users = {}
+    companies = {}
+    authenticateResponseStatus = 200
+    authenticateResponseMessage = nil
+  end
+  
+  post '/users' do
+
+    if(authenticateResponseStatus != 200 || authenticateResponseMessage != nil)
+      return [authenticateResponseStatus.to_i, {'Content-Type'=>'application/json'}, [authenticateResponseMessage.to_json]]
+    end
+
+    authHeader = request.env["HTTP_AUTHORIZATION"]
+    if(authHeader.nil? || !(authHeader.include? "Bearer"))
+      return [403]
+    end
+
+    base_uri = YAML.load(File.read("#{$PROJECT_ROOT}/endpoints/config/test_mock_service.yml"))[ENV['ENVIRONMENT']][:base_uri]
+
+    userRequest =  request.env["rack.request.form_hash"]
+
+    company = nil
+
+    if(!companies.key?userRequest["company"]["id"])
+      companyId = userRequest["company"]["id"]
+      companyHref = base_uri + "company/id/" + companyId
+      company = FactoryGirl.build(:company, href: companyHref, id: companyId)
+      companies[companyId]=company
+    else
+      company = companies[userRequest["company"]["id"]]
+    end
+
+    userId = SecureRandom.uuid
+    user = FactoryGirl.build(:user, href:base_uri + "users/id/" + userId, id: userId, token: '123', userName:  userRequest["userName"],
+                             emailAddress: userRequest["emailAddress"] , company_href: company.href, company_name: company.name,
+                            firstName: userRequest["firstName"], lastName: userRequest["lastName"], password: userRequest["password"],
+                             role: userRequest["role"], activeFlag: userRequest["activeFlag"])
+    users[user.id]=user
+    return [201, {'Location'=>base_uri+"/user/id/" + user.id},[]]
   end
 
   post '/users/authenticate/status' do
